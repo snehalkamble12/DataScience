f9875ad57f2c15e70d6249ec9f0fed4fd040db03
snehalkamble12 <156627313+snehalkamble12@users.noreply.github.com>
2024-01-18 20:55:43 +0530

    Create Electricity_bill.py

---
+++ Electricity_bill.py
@@ -0,0 +1,10 @@
+units = int(input("Enter the units"))
+if units <= 100:
+  print("No charge")
+elif units >= 100 and units<=200:
+  total_unit = units-100
+  print(total_unit * 5)
+else:
+  total_charges = 100 * 5
+  total_unit = units - 200
+  print(total_charges + total_unit * 10)
